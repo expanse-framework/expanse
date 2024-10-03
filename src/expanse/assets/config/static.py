0842d0792264a291e3f00a3672dc15e62ffcf56c
Sébastien Eustace <sebastien@eustace.io>
2024-10-04 01:16:39 +0200

    Re-add mising static setting

--- src/expanse/assets/config/static.py
+++ src/expanse/assets/config/static.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
 from pydantic import Field
+from pydantic import HttpUrl
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
 
 
 class Config(BaseSettings):
+    url: HttpUrl | None = None
     prefix: str = "/static"
     paths: list[Path] = Field(default=[Path("static")])
 
