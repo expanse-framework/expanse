89076a3599b749a897512364805a646f895be051
Sébastien Eustace <sebastien@eustace.io>
2025-03-14 18:52:02 +0100

    Improve cookie implementation

--- src/expanse/http/response.py
+++ src/expanse/http/response.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
 from datetime import datetime
+from datetime import timezone
 from typing import TYPE_CHECKING
-from typing import Literal
 from typing import Self
 
 from baize.asgi.responses import PlainTextResponse
 from baize.asgi.responses import Response as BaizeResponse
 from baize.asgi.responses import SmallResponse as BaizeSmallResponse
-from baize.datastructures import Cookie
+from baize.datastructures import Cookie as BaizeCookie
+
+from expanse.http.cookie import Cookie
+from expanse.http.cookie import SameSite
 
 
 if TYPE_CHECKING:
@@ -41,7 +44,7 @@ class Response:
         self._response = response
         self._status_code = response.status_code
         self._headers = response.headers
-        self._cookies = response.cookies
+        self._cookies: dict[str, Cookie] = {}
 
     @property
     def status_code(self) -> int:
@@ -52,8 +55,8 @@ class Response:
         return self._response.headers
 
     @property
-    def cookies(self) -> list[Cookie]:
-        return self._response.cookies
+    def cookies(self) -> dict[str, Cookie]:
+        return self._cookies
 
     @property
     def content_type(self) -> str | None:
@@ -92,49 +95,54 @@ class Response:
 
     def set_cookie(
         self,
-        key: str,
-        value: str = "",
-        max_age: int = -1,
-        expires: int | datetime | None = None,
-        path: str = "/",
+        name: str,
+        value: str | None = None,
+        expires: int | datetime = 0,
         domain: str | None = None,
+        path: str | None = None,
         secure: bool = False,
-        httponly: bool = False,
-        samesite: Literal["strict", "lax", "none"] = "lax",
-    ) -> None:
-        self._response.cookies.append(
-            Cookie(
-                key,
-                value,
-                expires=self._compute_expires(expires),
-                max_age=max_age,
-                path=path,
-                domain=domain,
-                secure=secure,
-                httponly=httponly,
-                samesite=samesite,
-            )
+        http_only: bool = False,
+        same_site: SameSite = SameSite.LAX,
+        partitioned: bool = False,
+    ):
+        """
+        :param name: The name of the cookie.
+        :param value: The value of the cookie.
+        :param expires: The time the cookie expires.
+        :param domain: The domain the cookie is available to.
+        :param path: The path on the server in which the cookie will be available on.
+        :param secure: Whether the client should send back the cookie only over HTTPS
+                       or None to auto-enable this when the request is already using HTTPS.
+        :param http_only: Whether the cookie will be made accessible only through the HTTP protocol.
+        :param same_site: Whether the cookie will be available for cross-site requests.
+        :param partitioned: Whether the cookie is partitioned or not.
+        """
+        self._cookies[name] = Cookie(
+            name,
+            value,
+            expires=expires,
+            domain=domain,
+            path=path,
+            secure=secure,
+            http_only=http_only,
+            same_site=same_site,
+            partitioned=partitioned,
         )
 
-    def delete_cookie(
-        self,
-        key: str,
-        value: str = "",
-        path: str = "/",
-        domain: str | None = None,
-        secure: bool = False,
-        httponly: bool = False,
-        samesite: Literal["strict", "lax", "none"] = "lax",
-    ) -> None:
-        self.set_cookie(
-            key,
-            value=value,
-            path=path,
-            domain=domain,
-            secure=secure,
-            httponly=httponly,
-            samesite=samesite,
-        )
+    async def _prepare(self) -> None:
+        for name, cookie in self._cookies.items():
+            self._response.cookies.append(
+                BaizeCookie(
+                    name,
+                    cookie.value,
+                    expires=cookie.expires,
+                    path=cookie.path,
+                    domain=cookie.domain,
+                    secure=cookie.is_secure(),
+                    httponly=cookie.is_http_only(),
+                    samesite=cookie.same_site,
+                )
+            )
 
     def _compute_expires(self, expires: int | datetime | None) -> datetime | None:
         if expires is None:
@@ -143,7 +151,9 @@ class Response:
         if isinstance(expires, datetime):
             return expires
 
-        return datetime.fromtimestamp(expires)
+        return datetime.fromtimestamp(expires, tz=timezone.utc)
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        await self._prepare()
+
         return await self._response(scope=scope, receive=receive, send=send)
