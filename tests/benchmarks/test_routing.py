ebec65142d462542470c63567bb92871cc8c4fc5
Sébastien Eustace <sebastien@eustace.io>
2025-07-01 22:41:35 +0200

    Add first benchmarks (#41)

---
+++ tests/benchmarks/test_routing.py
@@ -0,0 +1,30 @@
+from pytest_codspeed.plugin import BenchmarkFixture
+
+from expanse.contracts.routing.router import Router
+from expanse.http.helpers import json
+from expanse.http.response import Response
+from expanse.routing.helpers import get
+from expanse.testing.client import TestClient
+
+
+@get("/", name="index")
+async def index() -> Response:
+    return json({"message": "Hello world!"})
+
+
+class Controller:
+    @get("/controller", name="controller")
+    async def controller(self) -> Response:
+        return json({"message": "Hello from controller!"})
+
+
+def test_simple_routes(
+    router: Router, client: TestClient, benchmark: BenchmarkFixture
+) -> None:
+    benchmark(client.get, "/")
+
+
+def test_controller_route(
+    router: Router, client: TestClient, benchmark: BenchmarkFixture
+) -> None:
+    benchmark(client.get, "/controller")
