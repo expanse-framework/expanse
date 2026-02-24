ee4d4c80ab7d59657ded63a52821498cdc17158b
Sébastien Eustace <sebastien@eustace.io>
2026-02-24 18:38:40 +0100

    Add support for routing global loggers to channels

--- src/expanse/logging/logging_service_provider.py
+++ src/expanse/logging/logging_service_provider.py
@@ -1,5 +1,8 @@
+
 from collections.abc import AsyncGenerator
 
+from expanse.configuration.config import Config
+from expanse.container.container import Container
 from expanse.core.application import Application
 from expanse.logging.channel import LogChannel
 from expanse.logging.logger import Logger
@@ -10,6 +13,17 @@ class LoggingServiceProvider(ServiceProvider):
     async def register(self) -> None:
         self._container.singleton(Logger)
         self._container.singleton(LogChannel, self._create_channel)
+
+    async def boot(self, config: Config, container: Container) -> None:
+        logging_routing_config = config.get("logging", {}).get("routing", {})
+
+        if not logging_routing_config:
+            return
+
+        # If the logging routing configuration is defined, we need to ensure that the channels are created immediately, so that they can be used in the routing configuration.
+        logger = await container.get(Logger)
+        for logger_name in logging_routing_config.keys():
+            logger.route_base_logger(logger_name)
 
     async def _create_logger(self, app: Application) -> AsyncGenerator[Logger]:
         logger = Logger(app)
