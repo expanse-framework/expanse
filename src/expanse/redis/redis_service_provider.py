b4e4fc3156a10b76bd435ab846da110f54f11c46
Sébastien Eustace <sebastien@eustace.io>
2026-04-12 18:05:46 +0200

    Add sync Redis support

--- src/expanse/redis/redis_service_provider.py
+++ src/expanse/redis/redis_service_provider.py
@@ -1,26 +1,45 @@
 from collections.abc import AsyncGenerator
+from collections.abc import Generator
 from typing import TYPE_CHECKING
 
 from expanse.configuration.config import Config
-from expanse.redis.asynchronous.redis_manager import RedisManager
+from expanse.redis.asynchronous.redis_manager import RedisManager as AsyncRedisManager
+from expanse.redis.synchronous.redis_manager import RedisManager as SyncRedisManager
 from expanse.support.service_provider import ServiceProvider
 
 
 if TYPE_CHECKING:
-    from expanse.redis.asynchronous.connections.connection import Connection
+    from expanse.redis.asynchronous.connections.connection import (
+        Connection as AsyncConnection,
+    )
+    from expanse.redis.synchronous.connections.connection import (
+        Connection as SyncConnection,
+    )
 
 
 class RedisServiceProvider(ServiceProvider):
     async def register(self) -> None:
-        from expanse.redis.asynchronous.connections.connection import Connection
-        from expanse.redis.asynchronous.redis_manager import RedisManager
+        from expanse.redis.asynchronous.connections.connection import (
+            Connection as AsyncConnection,
+        )
+        from expanse.redis.asynchronous.redis_manager import (
+            RedisManager as AsyncRedisManager,
+        )
+        from expanse.redis.synchronous.connections.connection import (
+            Connection as SyncConnection,
+        )
+        from expanse.redis.synchronous.redis_manager import (
+            RedisManager as SyncRedisManager,
+        )
 
-        self._container.singleton(RedisManager, self._create_redis_manager)
-        self._container.singleton(Connection, self._create_connection)
+        self._container.singleton(AsyncRedisManager, self._create_async_redis_manager)
+        self._container.singleton(AsyncConnection, self._create_async_connection)
+        self._container.singleton(SyncRedisManager, self._create_sync_redis_manager)
+        self._container.singleton(SyncConnection, self._create_sync_connection)
 
-    async def _create_redis_manager(
+    async def _create_async_redis_manager(
         self, config: Config
-    ) -> AsyncGenerator["RedisManager", None]:
+    ) -> AsyncGenerator["AsyncRedisManager", None]:
         from expanse.redis.asynchronous.redis_manager import RedisManager
 
         manager = RedisManager(config)
@@ -29,11 +48,31 @@ class RedisServiceProvider(ServiceProvider):
 
         await manager.close()
 
-    async def _create_connection(
-        self, manager: RedisManager, name: str | None = None
-    ) -> AsyncGenerator["Connection", None]:
+    async def _create_async_connection(
+        self, manager: AsyncRedisManager, name: str | None = None
+    ) -> AsyncGenerator["AsyncConnection", None]:
         connection = await manager.connection(name)
 
         yield connection
 
         await connection.aclose()
+
+    def _create_sync_redis_manager(
+        self, config: Config
+    ) -> Generator["SyncRedisManager", None, None]:
+        from expanse.redis.synchronous.redis_manager import RedisManager
+
+        manager = RedisManager(config)
+
+        yield manager
+
+        manager.close()
+
+    def _create_sync_connection(
+        self, manager: SyncRedisManager, name: str | None = None
+    ) -> Generator["SyncConnection", None, None]:
+        connection = manager.connection(name)
+
+        yield connection
+
+        connection.close()
